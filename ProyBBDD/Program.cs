56de6e9269e27d1b352943a1701070e233b4ad1b
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-10-24 22:44:20 +0200

    Update Program.cs
    como si empezara el proyecto de nuevo

--- ProyBBDD/Program.cs
+++ ProyBBDD/Program.cs
@@ -10,57 +10,30 @@ namespace E_Commerce_VS
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+    
             // Add services to the container.
-
-            builder.Services.AddControllers().AddJsonOptions(options =>
-            {
-                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-            }
-            
-            );
+    
+            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
-            builder.Services.AddAuthentication()
-                .AddJwtBearer(options =>
-                {
-                    //antes del f "" 8 ya hay 24 caracteres (256 bits)
-                    //Debería establecerse una variable de entorno
-
-                    string key = "aljdvb##@coienwe82784f8fnuioecwcq2";
-
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        //Página 94 del PDF de Jose
-
-                        ValidateIssuer = false,
-
-                        ValidateAudience = false,
-
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
-                    };
-
-                });
-
+    
             var app = builder.Build();
-
+    
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+    
             app.UseHttpsRedirection();
-
-            app.MapControllers();
-
-            app.UseAuthentication();
-
+    
             app.UseAuthorization();
-
+    
+    
+            app.MapControllers();
+    
             app.Run();
         }
     }
