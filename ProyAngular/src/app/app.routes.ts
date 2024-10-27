fd94c26f555e9b98a96b92031c9566757eab265f
Fito-9 <aburbel032@g.educaand.es>
2024-10-27 15:54:00 +0100

    Cambios login y otras cosas

--- ProyAngular/src/app/app.routes.ts
+++ ProyAngular/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: MainComponent,
+    },
+
+    {
+        path: 'login',
+        component: LoginComponent,
     }
 ];
