d43167591dbe1a67f5f4b67e9522b726b1b5b843
Fito-9 <aburbel032@g.educaand.es>
2024-10-27 15:30:01 +0100

    Archivos staged

--- ProyBBDD/obj/Release/net8.0/E_Commerce_VS.AssemblyInfo.cs
+++ ProyBBDD/obj/Release/net8.0/E_Commerce_VS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b2298368365cb6ef4ab4562f8fe56037d02ec5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e2ab21e550302cc4ba8bbeb2213ab5ae840997")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
