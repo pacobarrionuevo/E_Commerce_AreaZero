a7adde08fe9633afd6fee8283b0ed5959d21fbb1
Fito-9 <aburbel032@g.educaand.es>
2024-10-31 19:24:32 +0100

    Cambios antes del login

--- ECommerce_Angular/src/app/pages/login/login.component.ts
+++ ECommerce_Angular/src/app/pages/login/login.component.ts
@@ -1,28 +1,40 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD:ProyAngular/src/app/login/login.component.ts
-<<<<<<< HEAD
-import { RouterModule } from '@angular/router';
-=======
->>>>>>> development
-=======
-import { RouterModule } from '@angular/router';
->>>>>>> origin/development:ECommerce_Angular/src/app/pages/login/login.component.ts
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { AuthRequest } from '../../models/auth-request';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-auth',
   standalone: true,
-<<<<<<< HEAD:ProyAngular/src/app/login/login.component.ts
-<<<<<<< HEAD
-  imports: [RouterModule],
-=======
-  imports: [],
->>>>>>> development
-=======
-  imports: [RouterModule],
->>>>>>> origin/development:ECommerce_Angular/src/app/pages/login/login.component.ts
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  name: string = '';
+  password: string = '';
+  role: string = 'admin';
+  jwt: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  async submit() {
+    const authData : AuthRequest = { username: this.name, password: this.password, role: this.role };
+    const result = await this.authService.login(authData);
+
+    if (result.success) {
+      this.jwt = result.data.accessToken;
+    }
+  }
+
+  async showSecretMessage() {
+    const result = await this.authService.getSecretMessage();
+
+    if (result.success) {
+      alert(result.data);
+    } else {
+      alert('No estás autorizado');
+    }
+  }
 }
