6e7e28c23378709a76f65304e0018606bb6c0981
gruiazu <gruiazu@g.educaand.es>
2024-11-03 22:19:58 +0100

    catalog+aboutus(test)

---
+++ ECommerce_Angular/src/app/models/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    id: number;
+    name: string;
+    ruta: string;
+
+    constructor(id: number, nombre: string, ruta: string) {
+        this.id = id;
+        this.name = nombre;
+        this.ruta = ruta;
+    }
+}
