53f2543272b86364fc03034caf4cb31572580983
Fito-9 <aburbel032@g.educaand.es>
2024-11-04 13:16:27 +0100

    Cambios en el catalogo v2

--- ECommerce_Angular/src/app/app.routes.ts
+++ ECommerce_Angular/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
-import { CatalogoComponent } from './catalogo/catalogo.component';
+import { CatalogComponent } from './catalog/catalogo.component';
 
 export const routes: Routes = [
     {
@@ -18,7 +18,7 @@ export const routes: Routes = [
         component: RegisterComponent,
     },
     {
-        path: 'Catalogo',
-        component: CatalogoComponent,
+        path: 'catalogo',
+        component: CatalogComponent,
     }
 ];
---
+++ ECommerce_Angular/src/app/models/products.ts
@@ -0,0 +1,5 @@
+export interface Products {
+    id: number;
+    nombre: string;
+    ruta: string;
+}
