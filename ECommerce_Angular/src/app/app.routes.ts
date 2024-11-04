0338385bbcc64156a7523879fb854ff208348f06
gruiazu <gruiazu@g.educaand.es>
2024-11-04 12:13:17 +0100

    aboutusfix

--- ECommerce_Angular/src/app/app.routes.ts
+++ ECommerce_Angular/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainComponent } from './pages/main/main.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,9 @@ export const routes: Routes = [
     {
         path: 'catalog',
         component: CatalogComponent,
+    },
+    {
+        path: 'aboutus',
+        component: AboutusComponent,
     }
 ];
