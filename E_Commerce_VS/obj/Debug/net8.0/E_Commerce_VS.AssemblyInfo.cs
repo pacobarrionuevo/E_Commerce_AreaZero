b0a16ae1a4251e5edad9f2f349302e3a65dac4d7
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-11-26 17:46:44 +0100

    commit no se porque

--- E_Commerce_VS/obj/Debug/net8.0/E_Commerce_VS.AssemblyInfo.cs
+++ E_Commerce_VS/obj/Debug/net8.0/E_Commerce_VS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04426ceb9de8212ea90c708295a032238c8bc2cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918c150b225f861e8df66ca40c594eb0003c9b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
