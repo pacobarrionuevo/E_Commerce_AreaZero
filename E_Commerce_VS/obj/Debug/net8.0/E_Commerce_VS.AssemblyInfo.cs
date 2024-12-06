592c0cb1607e25a2a03e344eb12bac70ee2e4559
gruiazu <gruiazu@g.educaand.es>
2024-12-06 20:35:12 +0100

    merge

--- E_Commerce_VS/obj/Debug/net8.0/E_Commerce_VS.AssemblyInfo.cs
+++ E_Commerce_VS/obj/Debug/net8.0/E_Commerce_VS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3ee1316c573812f67b59ff524d985a2b47aaba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60992da57b66f9712b94ee6cf51df92eff42f171")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
