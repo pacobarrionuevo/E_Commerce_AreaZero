abbd3a389ab94859e4cbedbef55a418fe28a57e0
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-12-06 16:02:14 +0100

    final de esta rama

--- E_Commerce_VS/obj/Debug/net8.0/E_Commerce_VS.AssemblyInfo.cs
+++ E_Commerce_VS/obj/Debug/net8.0/E_Commerce_VS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a66e6a7a122bcd612847a6703b4fc78bbf74cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852d9b52a84868cdbaf211e8844f5dd097d9bba5")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
