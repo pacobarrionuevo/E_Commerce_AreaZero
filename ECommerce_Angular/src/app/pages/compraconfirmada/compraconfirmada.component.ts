1bdef08d8503b055cdaba8f59518247fbd288114
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-12-11 03:50:40 +0100

    cambio a la rama de gonzalo xq ha petao el angular jajaja siuuuu

--- ECommerce_Angular/src/app/pages/compraconfirmada/compraconfirmada.component.ts
+++ ECommerce_Angular/src/app/pages/compraconfirmada/compraconfirmada.component.ts
@@ -2,29 +2,36 @@ import { Component } from '@angular/core';
 import { Carrito } from '../../models/carrito';
 import { AuthService } from '../../services/auth.service';
 import { CarritoService } from '../../services/carrito.service';
+import { ProductoCarrito } from '../../models/producto-carrito';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-compraconfirmada',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './compraconfirmada.component.html',
   styleUrl: './compraconfirmada.component.css'
 })
 export class CompraconfirmadaComponent {
-  productos: Carrito[] = [];
+  productosCarrito: ProductoCarrito[] = [];
 
 
   constructor(private authService: AuthService, private carritoService: CarritoService) {
-    this.obtenerNombreUsuario();
     this.cargarProductos();
   }
 
-  async obtenerNombreUsuario() {
-    this.name = await this.authService.getNameUser();
-  }
 
-  cargarProductos() {
-    this.carritoService.getProductosCarrito().subscribe((productos: CarritoEntero[]) => {
-      this.productos = productos;
-    });
+  async cargarProductos() {
+    try {
+      const result = await this.carritoService.getProductosCarrito();
+      if (result.success) {
+        this.productosCarrito = result.data;
+      } else {
+        console.error('Error al cargar productos:', result.error);
+      }
+    } catch (error) {
+      console.error('Error en el servidor:', error);
+    }
   }
 }
--- E_Commerce_VS/obj/Debug/net8.0/E_Commerce_VS.AssemblyInfo.cs
+++ E_Commerce_VS/obj/Debug/net8.0/E_Commerce_VS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76011876b47c52a192c000050e24bd51a0e4781")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485ce3e5c467b40284f8d72b67b45f0a0fc7f8a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Commerce_VS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
